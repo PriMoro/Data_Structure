564bc9b5af28b0e1561ad894f5e2e7cd3ffd8343
PriMoro <86128822+PriMoro@users.noreply.github.com>
2022-11-07 23:58:16 -0300

    scanner class to set values into vector

--- Arrays.java
+++ Arrays.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Arrays {
     public static void main(String args[]) {
         int vector[] = new int[5];
@@ -8,8 +10,19 @@ public class Arrays {
         vector[4] = 25;
 
         for (int i = 0; i < vector.length; i++) {
-            System.out.println("estoy en la posicion: " + i);
-            System.out.println("contiene el numero: " + vector[i]);
+            System.out.println("estoy en el lugar: " + i + " con el valor de " + vector[i]);
+        }
+
+        Scanner keyboard = new Scanner(System.in);
+
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println("Ingrese el valor para el lugar " + i);
+            int key = keyboard.nextInt();
+            vector[i] = key;
+        }
+
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println("estoy en el lugar: " + i + " con el valor de " + vector[i]);
         }
 
         int[][] ejemplo3x3 = new int[2][2];
